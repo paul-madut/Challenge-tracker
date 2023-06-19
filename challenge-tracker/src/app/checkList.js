55333219e93756e5a6388ad060b9d96467531b9f
paul-madut <paul.madut@mths.ca>
2023-06-18 23:39:43 -0400

    Create checkList.js

---
+++ challenge-tracker/src/app/checkList.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function checkList() {
+  return (
+    <div className=" w-full h-1/2">
+      <ul>
+        <li>
+          <input type="checkbox" name="" id="" /> Social Media Post
+        </li>
+        <li>
+          <input type="checkbox" name="" id="" /> 3 Github Commits
+        </li>
+        <li>
+          <input type="checkbox" name="" id="" /> 1 Leetcode Medium
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default checkList;
