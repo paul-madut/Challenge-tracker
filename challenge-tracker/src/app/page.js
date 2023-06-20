33d283b9ca1c539970cd9318e126807e2bc923a1
paul-madut <paul.madut@mths.ca>
2023-06-19 23:47:25 -0400

    Header bar

--- challenge-tracker/src/app/page.js
+++ challenge-tracker/src/app/page.js
@@ -3,7 +3,9 @@ import checkList from "./checkList";
 export default function Page() {
   return (
     <div>
-      <h1 className="text-6xl text-center">Paul Tracker</h1>
+      <div className="header__bar flex w-full bg-slate-600 justify-center shadow-xl">
+        <h1 className="text-6xl text-center">Paul Tracker</h1>
+      </div>
       <div className="points h-full p-4">
         <div className="points_grid h-1/2 grid sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5 text-center">
           <div className="points_grid_item border-2 border-black">
